c9ac8865327b85c687e3acc0a82ff9c04498c1c5
thefloatingtree <thefloatingtree@gmail.com>
2022-01-12 17:34:31 -0700

    dropdown: move styles to styled comp., fix click outside

--- src/stories/Dropdown/Dropdown.jsx
+++ src/stories/Dropdown/Dropdown.jsx
@@ -2,6 +2,10 @@ import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import Button from "../Button/Button"
 
+const DropdownContainer = styled.div`
+    width: fit-content;
+`
+
 const DropdownContentContainer = styled.div`
     position: absolute;
 
@@ -19,25 +23,32 @@ const RotateContainer = styled.div`
     ${ props => props.active && `transform: rotate(-90deg);` }
 `
 
+const ButtonContentContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+`
+
 export default function Dropdown({ label, isFullWidth = false, children }) {
     const [open, setOpen] = useState(false);
     const dropdownContainerRef = useRef(null)
     useOnClickOutside(dropdownContainerRef, () => setOpen(false))
 
     return (
-        <div ref={dropdownContainerRef}>
+        <DropdownContainer ref={dropdownContainerRef}>
             <Button onClick={() => setOpen(!open)} isFullWidth={isFullWidth}>
-                <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", width: "100%" }}>
+                <ButtonContentContainer>
                     <span>{label}</span>
                     <RotateContainer active={open}>
                         <svg style={{ width: "1rem", height: "1rem" }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                         </svg>
                     </RotateContainer>
-                </div>
+                </ButtonContentContainer>
             </Button>
             <DropdownContentContainer open={open}>{children}</DropdownContentContainer>
-        </div>
+        </DropdownContainer>
     );
 }
 
