e858386b7de468aeb9c6aaf9909b40c80f0804b6
thefloatingtree <thefloatingtree@gmail.com>
2022-01-19 15:48:58 -0700

    add TextArea

---
+++ src/stories/TextArea/TextArea.jsx
@@ -0,0 +1,38 @@
+import ExpandingTextArea from 'react-expanding-textarea'
+import styled from 'styled-components'
+
+const StyledExpandingTextArea = styled(ExpandingTextArea)`
+    border-radius: 0.5rem;
+    border-color: black;
+    padding: 0.75rem 1rem;
+    border-width: 3px;
+    font-weight: 600;
+    background-color: white;
+`
+
+const StyledTextArea = styled.textarea`
+    border-radius: 0.5rem;
+    border-color: black;
+    padding: 0.75rem 1rem;
+    border-width: 3px;
+    font-weight: 600;
+    background-color: white;
+`
+
+export default function TextArea({ value, onChange, rows = 3, placeholder, maxLength, resize = true, resizeDirection = 'both', expand = false, children }) {
+
+    const Component = expand ? StyledExpandingTextArea : StyledTextArea;
+    resizeDirection = expand ? 'horizontal' : resizeDirection;
+
+    return (
+        <Component
+            defaultValue={children}
+            value={value}
+            onChange={onChange}
+            rows={rows}
+            placeholder={placeholder}
+            maxLength={maxLength}
+            style={{ resize: resize ? resizeDirection : 'none' }}
+        />
+    )
+}
