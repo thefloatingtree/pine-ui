8332d651d7c1bfa3a18f6cd001d8469061593728
thefloatingtree <thefloatingtree@gmail.com>
2022-01-24 14:31:52 -0700

    add spinner

---
+++ src/stories/Spinner/Spinner.stories.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../../index.css";
+import Spinner from './Spinner';
+
+export default {
+    title: "Spinner",
+    component: Spinner
+}
+
+const Template = (args) => <Spinner />
+
+export const Default = Template.bind({})
+Default.args = {
+}
